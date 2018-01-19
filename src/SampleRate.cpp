56d75f02ca3bf7f43d5a6a6f58a3f9ad407a4e96
@cidvertigo <ldanzelmo@gmail.com>
2018-01-19 21:14:46 +0100

    Add samplerate interface implementation

---
+++ src/SampleRate.cpp
@@ -0,0 +1,38 @@
+/*
+ * File: SampleRate.cpp
+ * ---------------
+ * This file implements the SampleRate.h interface.
+ */
+#include <string>
+#include "SampleRate.h"
+
+SampleRate::SampleRate() {
+   SampleRate = 0;
+}
+SampleRate::SampleRate(int sampleratec) {
+   samplerate = sampleratec;
+}
+
+int SampleRate::getX() {
+   return x;
+}
+
+int SampleRate::getY() {
+   return y;
+}
+
+string SampleRate::toString() {
+   return "(" + integerToString(x) + "," + integerToString(y) + ")";
+}
+
+bool operator==(SampleRate s1, SampleRate s2) {
+   return p1.x == p2.x && p1.y == p2.y;
+}
+
+bool operator!=(SampleRate s1, SampleRate s2) {
+   return !(p1 == p2);
+}
+
+ostream & operator<<(ostream & os, SampleRate sr) {
+   return os << sr.toString();
+}
