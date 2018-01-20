04a0d7ee6dfdc9e13fe939288a20a33faf40cd82
@cidvertigo <ldanzelmo@gmail.com>
2018-01-20 18:53:16 +0100

    Add range lib

--- include/common/utils/libString.h
+++ include/common/utils/libString.h
@@ -8,12 +8,28 @@
 
 #include <sstream>
 
+/* Usage: NumberToString(Number)
+ * -------------
+ * Template to comvert integer to string
+ */
 template <typename T>
   std::string NumberToString ( T Number )
   {
      std::ostringstream ss;
      ss << Number;
      return ss.str();
+  }
+
+/* Usage: StringToNumber<Type> ( String );
+ * -------------
+ * Template to comvert integer to string
+ */
+template <typename T>
+  T StringToNumber ( const string &Text )
+  {
+     istringstream ss(Text);
+     T result;
+     return ss >> result ? result : 0;
   }
 
 #endif /* _LIBSTR_H_ */
