8a5773d22cfdea5e99c30d8ffaa22dd609970f9f
@cidvertigo <ldanzelmo@gmail.com>
2018-01-20 23:00:08 +0100

    Fix namespace

--- test/SampleRateTest.cpp
+++ test/SampleRateTest.cpp
@@ -1,11 +1,7 @@
 #include "SampleRate.h"
 #include "SampleRateTest.h"
 
-namespace Test {
-  
-  int SampleRateTest() {
+int Test::SampleRateTest() {
     SampleRate obj = new SampleRate();
     static_assert(obj == 0, "SampleRate ctor does not set 0 to samplerate property");
-  }
-  
 }
