bb8e5e31ff35bff13586e3a934298607f11e38fc
@cidvertigo <ldanzelmo@gmail.com>
2018-01-20 10:39:38 +0100

    Add libstr lib utility

---
+++ lib/util/libString.h
@@ -0,0 +1,11 @@
+/*
+ * File: libStr.h
+ * -------------
+ * This interface exports the libStr utility functions to work with strings
+ */
+#ifndef _LIBSTR_H_
+#define _LIBSTR_H_
+
+
+
+#endif /* _LIBSTR_H_ */
