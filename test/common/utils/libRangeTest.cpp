4c316d4979bf0d4c32c610698f94e7fe8890e736
@cidvertigo <ldanzelmo@gmail.com>
2018-01-21 10:48:08 +0100

    Add return statements

--- test/common/utils/libRangeTest.cpp
+++ test/common/utils/libRangeTest.cpp
@@ -3,10 +3,12 @@
 
      int Test::Common::Utils::containsTest() {
         static_assert(range<int>(1,10).contains(5), "Unable to find if integer 5 is in range (1,10)");
+        return 0;
       }
 
       int Test::Common::Utils::notContainsTest() {
         static_assert(range<int>(1,10).notContains(5),  "Unable to find if integer 5 is not in range (1,10)");
+        return 0;
       }
 
       int Test::Common::Utils::iteratorTest() {
