e8f172c539ddef7423221464fd966d715570cfcd
Luca <ldanzelmo@gmail.com>
2018-01-26 16:11:15 +0100

    Update ISampleRate.h

--- include/ISampleRate.h
+++ include/ISampleRate.h
@@ -35,7 +35,7 @@ public:
  * ----------------------------------
  * Returns a string representation of the samplerate.
  */
-   virtual std::string toString();
+   virtual std::string toString() const;
   
    friend bool operator==(const ISampleRate &sr1, const ISampleRate &sr2);
 };
