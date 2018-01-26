28b82b1b1f35aef49d8fbccf5e9b279537b4ca97
Luca <ldanzelmo@gmail.com>
2018-01-26 15:45:11 +0100

    Update SampleRate.cpp

--- src/SampleRate.cpp
+++ src/SampleRate.cpp
@@ -34,7 +34,7 @@ public:
 };
 
 bool operator==(const SampleRate &s1, const SampleRate &s2) {
- return s1.samplerate == s2.samplerate;
+ return s1.getSamplerate() == s2.getSamplerate();
 }
 
 bool operator!=(const SampleRate &s1, const SampleRate &s2) {
