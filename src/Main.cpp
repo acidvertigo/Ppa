676f0d15a2cff0446a39e49ffcf5985b0b816090
Luca <ldanzelmo@gmail.com>
2018-02-07 21:41:06 +0100

    Test comparison with pointers

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,17 +4,19 @@
  */
 
 #include <iostream>
+
 #include "audio/SampleRate.h"
 
 using namespace std;
 using namespace Ppa::Audio;
 
 int main() {
-  SampleRate sm(44100);
-  SampleRate sm2(44100);
-  bool verdict = sm == sm2;
+  SampleRate * sm(44100);
+  SampleRate * sm2(44100);
+
+  bool verdict = *sm == *sm2;
   cout << "Hello World!" << endl;
-  cout << "Samplerate value is: " << sm.GetSampleRate() << endl;
+  cout << "Samplerate value is: " << sm->GetSampleRate() << endl;
   cout << verdict << endl;
   return 0;
 }
