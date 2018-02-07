7f12d39c9911c17aeff4ee5a2ba6987b5f09a0e6
Luca <ldanzelmo@gmail.com>
2018-02-07 14:08:07 +0100

    Create samplerate_test.cpp

---
+++ test/audio/samplerate_test.cpp
@@ -0,0 +1,27 @@
+
+using namespace ::Foo;
+
+class SampleRateTest : public ::testing::Test
+{
+public:
+    SampleRateTest() {}
+    virtual ~SampleRateTest() {}
+
+protected:
+     Shared * sm;
+ 
+     virtual void SetUp() override
+     {      
+         sm = new SampleRate(11);
+     }
+
+     virtual void TearDown() override
+     {
+        delete sm;
+     }
+};
+
+TEST_F(SampleRateTest, smTest)
+{
+    ASSERT_EQ(sm->getSampleRate(), 11);
+}
