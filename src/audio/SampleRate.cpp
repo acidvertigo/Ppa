623922fd3127852b6ee04e8f4e166cbefe3317a6
Luca <ldanzelmo@gmail.com>
2018-02-07 10:52:45 +0100

    Create SampleRate.cpp

---
+++ src/audio/SampleRate.cpp
@@ -0,0 +1,44 @@
+/*
+ * File: SampleRate.cpp
+ * ---------------
+ * This file implements the SampleRate.h interface.
+ */
+#include "audio/SampleRate.h"
+#include "common/String.h"
+
+using namespace std;
+using namespace Ppa::Audio;
+
+/*
+ * Constructor: SampleRate
+ * Usage: Samplerate sr(sampleratec);
+ * ------------------------
+ * Creates a SampleRate object.  The parameter sets the samplerate;
+ */
+  SampleRate::SampleRate(int sampleratec) : samplerate(0) {
+    samplerate = sampleratec;
+  }
+
+  SampleRate::~SampleRate() = default;
+
+  int SampleRate::getSamplerate() const {
+    return samplerate;
+  }
+
+  std::string SampleRate::toString() const {
+    return NumberToString(samplerate);
+  }
+
+//------------------------------------
+
+  bool operator==(const SampleRate &s1, const SampleRate &s2) {
+    return s1.samplerate == s2.samplerate;
+  }
+
+  bool operator!=(const SampleRate &s1, const SampleRate &s2) {
+    return !(s1 == s2);
+  }
+
+  std::ostream & operator<<(ostream & os, const SampleRate &sr) {
+    return os << sr.toString();
+  }
