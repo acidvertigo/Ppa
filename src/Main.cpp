ed6bf6369cda253b658aceca11ece001752c4d64
Luca <ldanzelmo@gmail.com>
2018-02-07 21:53:05 +0100

    Code clean

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,13 +11,13 @@ using namespace std;
 using namespace Ppa::Audio;
 
 int main() {
-  SampleRate * sm;
+  SampleRate * sm
   SampleRate * sm2;
 
   sm  = new SampleRate(44100);
   sm2 = new SampleRate(44100);
 
-  bool verdict = *sm == *sm2;
+  bool verdict = (*sm == *sm2);
   cout << "Hello World!" << endl;
   cout << "Samplerate value is: " << sm->GetSampleRate() << endl;
   cout << verdict << endl;
