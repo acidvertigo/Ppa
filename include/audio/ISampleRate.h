1d736c2fe730ab4fea16cbfe38663f3b2d410378
Luca <ldanzelmo@gmail.com>
2018-02-07 10:40:31 +0100

    Create ISampleRate.h

---
+++ include/audio/ISampleRate.h
@@ -0,0 +1,41 @@
+/*
+ * File: ISampleRate.h
+ * -------------
+ * Interface for the SampleRate class.
+ */
+#ifndef _ISAMPLERATE_H_
+#define _ISAMPLERATE_H_
+
+#include <string>
+
+namespace Ppa {
+namespace Audio {
+
+class ISampleRate {
+  
+public:
+
+   virtual ~ISampleRate() = default;
+
+/*
+ * Methods: getSampleRate
+ * Usage: int x = sr.getSampleRate();
+ * ----------------------------------
+ * These methods returns the samplerate set
+ */
+   virtual int getSamplerate() const = 0;
+
+/*
+ * Method: toString
+ * Usage: string str = pt.toString();
+ * ----------------------------------
+ * Returns a string representation of the samplerate.
+ */
+   virtual std::string toString() const = 0;
+
+};
+
+} // namespace Audio end
+} // namespace Ppa end
+
+#endif /* _ISAMPLERATE_H_ */
