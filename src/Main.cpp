b86172752ee0268e2aaebfb71bdea07612772838
Luca <ldanzelmo@gmail.com>
2018-02-07 21:35:43 +0100

    Check comaparison op

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,8 +12,9 @@ using namespace Ppa::Audio;
 int main() {
   SampleRate sm(44100);
   SampleRate sm2(44100);
+  bool verdict = sm == sm2;
   cout << "Hello World!" << endl;
   cout << "Samplerate value is: " << sm.GetSampleRate() << endl;
-  cout << sm == sm2 << endl;
+  cout << verdict << endl;
   return 0;
 }
