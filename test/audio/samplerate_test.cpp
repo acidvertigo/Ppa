f9cb1636df21bd7fd4d953511e975d8e258c8bbf
Luca <ldanzelmo@gmail.com>
2018-02-09 14:38:42 +0100

    Update samplerate_test.cpp

--- test/audio/samplerate_test.cpp
+++ test/audio/samplerate_test.cpp
@@ -59,7 +59,7 @@ TEST_F(SampleRateTest, testNotEqual) {
 TEST_F(SampleRateTest, testSstream) {
     std::stringstream out;
     SampleRateTest samp
-    out << samp;
+    std::out << samp;
     ASSERT_TRUE(out.str() == "44100");
 }
 
