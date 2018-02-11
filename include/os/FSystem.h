5daa3001d640a212e9b0898a7891e60b9785a68f
Luca <ldanzelmo@gmail.com>
2018-02-11 18:05:01 +0100

    Rename export

--- include/os/FSystem.h
+++ include/os/FSystem.h
@@ -18,10 +18,10 @@ namespace Os {
 
 class FSystem : IFSystem {
 
-   FSYSTEM_EXPORT void Open (const char* filename, std::ios::openmode mode = std::ios::in | std::ios::out) const override;
-   FSYSTEM_EXPORT void Close () const override;
-   FSYSTEM_EXPORT bool IsOpen () const override;
-   FSYSTEM_EXPORT bool Exists () const = override;
+   OS_EXPORT void Open (const char* filename, std::ios::openmode mode = std::ios::in | std::ios::out) const override;
+   OS_EXPORT void Close () const override;
+   OS_EXPORT bool IsOpen () const override;
+   OS_EXPORT bool Exists () const = override;
 
 };
 
