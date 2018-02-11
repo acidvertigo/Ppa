30270008441adf223a7acce3497fcc6bb2c2514f
Luca <ldanzelmo@gmail.com>
2018-02-11 22:13:10 +0100

    Implementing constructor and is_open methods

--- src/os/FSystem.cpp
+++ src/os/FSystem.cpp
@@ -8,13 +8,22 @@
 using namespace Ppa::Os;
 using namespace std;
 
-   FSystem::FSystem(string filenamec) {}
+   FSystem::FSystem(string filenamec) {
+       filename = filenamec;
+   }
+
    ~ FSystem::FSystem() = default;
 
-   void FSystem::OpenFile(const char* filename, ios::openmode mode) const {
+   bool FSystem::FileIsOpen () const {
+       std::ifstream ifs (filename);
+       return ifs.is_open();
    }
-   void FSystem::CloseFile () const {}
-   bool FSystem::FileIsOpen () const {}
+
    bool FSystem::FileExists () const {}
    void WriteContent ( const char* filename, std::vector<BYTE> data) const {}
    vector<BYTE> ReadContent (const char* filename) const {}
+
+   void FSystem::OpenFile(const char* filename, ios::openmode mode) const {
+
+   }
+   void FSystem::CloseFile () const {}
