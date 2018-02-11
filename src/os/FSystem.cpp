a03eb92815262e07ac80fada0af2b25debd0b8d8
Luca <ldanzelmo@gmail.com>
2018-02-11 18:15:31 +0100

    Fix typo

--- src/os/FSystem.cpp
+++ src/os/FSystem.cpp
@@ -9,7 +9,7 @@ using namespace Ppa::Os;
 using namespace std;
 
    void FSystem::Open(const char* filename, ios::openmode mode = ios::in | ios::out) const {
-   }
+   };
    void FSystem::Close () const {};
    bool FSystem::IsOpen () const {};
    bool FSystem::Exists () const = {};
