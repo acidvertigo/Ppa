64c6c1f6efa2025498302a0f8e3dd95d1ea64f4d
Luca <ldanzelmo@gmail.com>
2018-02-11 17:44:03 +0100

    Fix namespace

--- src/fsystem/FSystem.cpp
+++ src/fsystem/FSystem.cpp
@@ -6,9 +6,9 @@
 #include "fsystem/FSystem.h"
 
 using namespace Ppa::FSystem;
-using namespace std::ios::openmode;
-using namespace std::ios::in;
-using namespace std::ios::out;
+using namespace std::ios_base::openmode;
+using namespace std::ios_base::in;
+using namespace std::ios_base::out;
 
 
    void Open (const char* filename, openmode mode = in | out) const {
