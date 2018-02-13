b9fc38993369ba512f0dc1a43ffb3824de2bd151
Luca <ldanzelmo@gmail.com>
2018-02-13 22:43:59 +0100

    Add test

---
+++ test/os/fsystem_test.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "os/FSystem.h"
+
+using namespace ::Ppa::os;
+
+//----------------------------------------
+class FsystemTest : public ::testing::Test {
+ public:
+  FsystemTest() {}
+  ~ FsystemTest() {}
+
+ protected:
+  FSystem* fs, fs2;
+
+  virtual void SetUp() override {
+    fs = new FSystem("piano.waw");
+    fs2 = new FSystem("piano.wav");
+  }
+
+  virtual void TearDown() override {
+    delete fs;
+    delete fs2;
+  }
+};
+
+TEST_F(ChannelTest, testGetFileName) {
+  EXPECT_EQ(ch->GetFilename(), "piano.wav");
+}
+
